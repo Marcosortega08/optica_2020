7de222c300bdca09da6ba038a8395226752018c2
Marcosortega08 <marcos.ortega08@inacapmail.cl>
2021-01-08 18:59:33 -0300

    ultimo

--- models/Conexion.php
+++ models/Conexion.php
@@ -4,9 +4,9 @@ namespace models;
 
 class Conexion
 {
-    public static $user = "root";
-    public static $pass = "";
-    public static $URL = "mysql:host=localhost;dbname=optica_2020";
+    public static $user = "uslhxvoijyjxgvgh";
+    public static $pass = "08LVIbtN1rh6LQS0XKQl";
+    public static $URL = "mysql:host=b73ic08qv8xvfesxflby-mysql.services.clever-cloud.com;dbname=b73ic08qv8xvfesxflby";
 
     //public static $user = "root";
     //public static $pass = "";
